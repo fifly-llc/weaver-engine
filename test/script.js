1a2ae7ceb728a4aca36a3462186848219ec729df
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-09-12 05:50:12 +1000

    v1.0.3

---
+++ test/script.js
@@ -0,0 +1,10 @@
+import 'https://cdn.jsdelivr.net/npm/weaver-engine@latest/src/index.js';
+
+let canvas = document.createElement('canvas');
+canvas = document.body.appendChild(canvas);
+canvas.style = "width: 100%; height: 100%; border: none; background-color: white;";
+
+let game = new Game(canvas);
+let cube = new GameObject(0, 0, 0, 10, 10, 10);
+game.add(cube);
+game.start();
