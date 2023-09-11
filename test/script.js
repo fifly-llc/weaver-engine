d11086f49b467c6d72b9cb13874289a9a2763cdf
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-09-12 06:14:02 +1000

    fix an issue

--- test/script.js
+++ test/script.js
@@ -1,10 +1,10 @@
-import { Game, GameObject } from 'https://cdn.jsdelivr.net/npm/weaver-engine@latest/src/index.js';
+import * as weaver from 'https://cdn.jsdelivr.net/npm/weaver-engine@latest/src/index.js';
 
 let canvas = document.createElement('canvas');
 canvas = document.body.appendChild(canvas);
 canvas.style = "width: 100%; height: 100%; border: none; background-color: white;";
 
-let game = new Game(canvas);
-let cube = new GameObject(0, 0, 0, 10, 10, 10);
+let game = new weaver.Game(canvas);
+let cube = new weaver.GameObject(0, 0, 0, 10, 10, 10);
 game.add(cube);
 game.start();
